d4ff7acb2647eea8bbe5d90a9a70da496cab3b4c
rominarodriguez <rominarodrisan@gmail.com>
2023-10-23 09:56:59 -0400

    fifth commit: corrected movement script and jump restrictions

--- Assets/Scripts_Temp/movement.cs
+++ Assets/Scripts_Temp/movement.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
+    public float speed;
+    public float jump;
+
+    private float Move;
+
+    public Rigidbody2D rb;
+
+    public bool isJumping;
 
     void Update()
     {
-        // Get input from the WASD keys
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
 
-        // Calculate the movement vector
-        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput) * moveSpeed * Time.deltaTime;
+        Move = Input.GetAxis("Horizontal");
 
-        // Apply the movement to the object's position
-        transform.Translate(movement);
+        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isJumping == false)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            isJumping = true;
+        }
     }
 }
