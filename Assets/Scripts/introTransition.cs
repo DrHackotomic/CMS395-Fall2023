545b3da2e485a38bf90f600a72eadadb9eaa7d31
rominarodriguez <rominarodrisan@gmail.com>
2023-11-17 18:33:30 -0500

    implemented scene transition: into->level 1, 15 sec delay

---
+++ Assets/Scripts/introTransition.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class introTransition : MonoBehaviour
+{
+
+   
+    public string nextLevelName = "level1";
+    public float delayInSeconds = 15f;
+
+    private bool canLoadNextLevel = false;
+
+    private void Start()
+    {
+    
+        // Start the timer to enable the trigger after the specified delay
+        Invoke("EnableLoadNextLevel", delayInSeconds);
+        Debug.Log("delay started");
+    }
+
+    private void EnableLoadNextLevel()
+    {
+      
+        // Enable the trigger to load the next level
+        canLoadNextLevel = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (canLoadNextLevel && collision.CompareTag("player"))
+        {
+            // Player has collided with the triggerable boundary, load the next level
+            SceneManager.LoadScene(nextLevelName);
+        }
+    }
+}
---
+++ Assets/Scripts/tempboundary.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempboundary : MonoBehaviour
+{
+    public float delayInSeconds = 15f;
+
+    private void Start()
+    {
+        // Invoke the method to make the object disappear after the specified delay
+        Invoke("DisappearObject", delayInSeconds);
+    }
+
+    private void DisappearObject()
+    {
+        // Deactivate (make invisible) the GameObject
+        gameObject.SetActive(false);
+        Debug.Log("object gone teehee");
+    }
+}
